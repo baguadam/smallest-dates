57863c56edeca9ed9818ecaabe9322a8192a0f56
Bagu Ádám <m3wdo5@inf.elte.hu>
2024-04-29 16:49:47 +0200

    Add skeleton of main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <fstream>
+#include "./headers/date.h"
+#include "./headers/timed_task.h"
 
-int main() {
+int main(int argc, char* argv[]) {
+    // if less or more than 1 argument
+    if (argc != 2) {
+        std::cerr << "The provided number of arguments is incorrect! \n";
+        return -1;
+    }
 
+    char* file_name = argv[1];
+    std::ifstream file(file_name);
+    if (!file) {
+        std::cerr << "File opening failed! \n";
+        return -1;
+    }
 
 
     return 0;
